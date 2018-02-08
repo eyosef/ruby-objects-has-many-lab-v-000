db4789450ad2e8ab95d875691e130de4ba156063
Eden Mesfun <emesfun@gmail.com>
2018-02-08 13:57:32 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,9 +21,9 @@ class Artist
 
   def add_song_by_name(name) #FIX
     song = Song.new(name)
-    @@song_count += 1
     song.artist = self
     @songs << song
+    @@song_count += 1
   end
 
   def self.song_count
