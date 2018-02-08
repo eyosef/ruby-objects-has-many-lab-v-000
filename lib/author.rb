6bb3a9ff783faeb5874ee9b8c22ffd7f6b644bfa
Eden Mesfun <emesfun@gmail.com>
2018-02-08 14:53:09 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,7 +19,7 @@ class Author
     @@post_count += 1
   end
 
-  def add_post_by_title(title) #FIX
+  def add_post_by_title(name) #FIX
     post = Post.name
     @posts << post
     @@post_count += 1
